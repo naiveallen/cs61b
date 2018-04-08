4859148e537bb1ea9e6e80e592da15380f04cb2b
unknown <pyyuanye@gmail.com>
2018-04-08 21:55:50 +0800

    complete lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Yuan Ye
  */
 public class LeapYear {
 
@@ -28,6 +28,14 @@ public class LeapYear {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
         }
+    }
+
+    public static boolean isLeapYear(int year){
+    	if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+    		return true;
+    	}else{
+    		return false;
+    	}
     }
 }
 
